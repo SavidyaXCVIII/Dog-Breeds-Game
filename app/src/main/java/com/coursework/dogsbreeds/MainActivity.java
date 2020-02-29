76c9284a081c00622d882e59fc418a4205430b3d
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-02-29 19:24:24 +0530

    Completed the Functionalities of the Main Activity

--- app/src/main/java/com/coursework/dogsbreeds/MainActivity.java
+++ app/src/main/java/com/coursework/dogsbreeds/MainActivity.java
@@ -3,12 +3,38 @@ package com.coursework.dogsbreeds;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private TextView difficulty;
+
+    private static String difficulty_level = "You have chosen Beginner Level";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        get difficulty text view
+        difficulty = (TextView)findViewById(R.id.difficulty_description);
+    }
+
+    //change difficulty level
+    public void changeDifficulty(View view) {
+//        adding a log message
+        Log.d(LOG_TAG, "Button Clicked (Changed Difficulty)");
+
+        if (difficulty_level.equals("You have chosen Beginner Level")){
+            difficulty_level = "You have chosen Intermediate Level";
+        }else {
+            difficulty_level = "You have chosen Beginner Level";
+        }
+//        set message
+        difficulty.setText(difficulty_level);
     }
 }

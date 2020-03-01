52057d94a94a8d44d22a9acc6440bae308632e6d
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-03-01 19:53:01 +0530

    Completed IdentifyDogBreed Functionality
    +added all the images

--- app/src/main/java/com/coursework/dogsbreeds/IdentifyBreedActivity.java
+++ app/src/main/java/com/coursework/dogsbreeds/IdentifyBreedActivity.java
@@ -2,13 +2,114 @@ package com.coursework.dogsbreeds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class IdentifyBreedActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class IdentifyBreedActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static HashMap<String, String[]> imagesMap;
+
+    private static String imageName;
+
+    private static String breedName;
+
+    private static String spinnerLabel;
+
+    private TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify_breed);
+
+        Intent intent = getIntent();
+
+        imagesMap = (HashMap<String, String[]>) intent.getSerializableExtra("Images");
+
+        ImageView breedImage = findViewById(R.id.breed_image);
+
+        imageName = getImageName();
+
+        int resource_id = getResources().getIdentifier(imageName, "drawable", "com.coursework.dogsbreeds");
+        breedImage.setImageResource(resource_id);
+
+        Spinner spinner = findViewById(R.id.dog_names_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.dog_names_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+        answer = (TextView)findViewById(R.id.result);
+    }
+
+    public String getImageName() {
+        String imageName = null;
+        Random random = new Random();
+
+        int randKey = random.nextInt(10);
+
+        Set<String> keys = imagesMap.keySet();
+        String[] breedNamesArray = keys.toArray(new String[keys.size()]);
+        breedName = breedNamesArray[randKey];
+        String[] imageNamesArray = imagesMap.get(breedNamesArray[randKey]);
+        int randValue = random.nextInt(5);
+        imageName = imageNamesArray[randValue];
+        System.out.println(imageName);
+
+        return imageName;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        spinnerLabel = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void checkAnswer(View view) {
+
+        String answerDescription;
+        if (spinnerLabel.equals(breedName)) {
+            answerDescription = "Your answer is CORRECT!";
+            answer.setTextColor(this.getResources().getColor(R.color.ColorGreen));
+        }
+        else {
+            answerDescription = "Your answer is WRONG";
+            answer.setTextColor(this.getResources().getColor(R.color.ColorRed));
+        }
+
+        answer.setText(answerDescription);
+
     }
 }
--- app/src/main/java/com/coursework/dogsbreeds/MainActivity.java
+++ app/src/main/java/com/coursework/dogsbreeds/MainActivity.java
@@ -2,10 +2,16 @@ package com.coursework.dogsbreeds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
     private static String difficulty_level = "You have chosen Beginner Level";
 
+    private static HashMap<String, String[]> imagesMap = new HashMap<>();
+
+    public void addImages(){
+        String[] bullMastiff = new String[]{"bull1", "bull2", "bull3", "bull4", "bull5"};
+        String[] doberman = new String[]{"doberman1", "doberman2", "doberman3", "doberman4", "doberman5"};
+        String[] eskimo = new String[]{"eskimo1", "eskimo2", "eskimo3", "eskimo4", "eskimo5"};
+        String[] germanShepard = new String[]{"german1", "german2", "german3", "german4", "german5"};
+        String[] goldenRetriever = new String[]{"golden1", "golden2", "golden3", "golden4", "golden5"};
+        String[] siberianHusky = new String[]{"husky1", "husky2", "husky3", "husky4", "husky5"};
+        String[] komondor = new String[]{"komondor1", "komondor2", "komondor3", "komondor4", "komondor5"};
+        String[] labradorRetriever = new String[]{"labrador1", "labrador2", "labrador3", "labrador4", "labrador5"};
+        String[] rottweiler = new String[]{"rotweiller1", "rotweiller2", "rotweiller3", "rotweiller4", "rotweiller5"};
+        String[] saint = new String[]{"saint1", "saint2", "saint3", "saint4", "saint5"};
+
+        imagesMap.put("Bull Mastiff", bullMastiff);
+        imagesMap.put("Doberman", doberman);
+        imagesMap.put("Eskimo", eskimo);
+        imagesMap.put("German Shepard", germanShepard);
+        imagesMap.put("Golden Retriever", goldenRetriever);
+        imagesMap.put("Siberian Husky", siberianHusky);
+        imagesMap.put("Komondor", komondor);
+        imagesMap.put("Labrador Retriever", labradorRetriever);
+        imagesMap.put("Bull Rottweiler", rottweiler);
+        imagesMap.put("Saint", saint);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
 //        get difficulty text view
         difficulty = (TextView)findViewById(R.id.difficulty_description);
+
+        addImages();
     }
 
     //change difficulty level
@@ -37,4 +71,32 @@ public class MainActivity extends AppCompatActivity {
 //        set message
         difficulty.setText(difficulty_level);
     }
+
+    public void launchIdentifyBreedActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked");
+
+        Intent intent = new Intent(this, IdentifyBreedActivity.class );
+        intent.putExtra("Images", imagesMap);
+        startActivity(intent);
+
+    }
+
+    public void launchIdentifyDogActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked");
+
+        Intent intent = new Intent(this, IdentifyDogsActivity.class );
+        intent.putExtra("Images", imagesMap);
+        startActivity(intent);
+
+    }
+
+    public void launchSearchBreedsActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked");
+        Intent intent = new Intent(this, SearchDogBreedsActivity.class);
+        intent.putExtra("Images", imagesMap);
+        startActivity(intent);
+
+    }
 }
+
+
